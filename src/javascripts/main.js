bea642b27270be213ac1bc01e6ecdc70a1a70ed9
Mary West <westmarye1219@gmail.com>
2019-05-20 19:54:59 -0500

    initialized firebase

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,9 +1,15 @@
+import firebase from 'firebase/app';
+
 import Auth from './components/auth/auth';
 import Birfday from './components/birfday/birfday';
+
+import apiKeys from './helpers/apiKeys.json';
 
 import '../styles/main.scss';
 
 const init = () => {
+  console.error('keys', apiKeys.firebaseKeys);
+  firebase.initializeApp(apiKeys.firebaseKeys);
   Auth.authPrint();
   Birfday.birfdayPrint();
 };
